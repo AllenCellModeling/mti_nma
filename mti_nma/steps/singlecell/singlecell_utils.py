42a07eb2be794df3aad737af622ee1630ab91d03
Julie <juliec@alleninstitute.org>
2020-02-17 09:00:16 -0800

    Fix Mac filepaths

--- mti_nma/steps/singlecell/singlecell_utils.py
+++ mti_nma/steps/singlecell/singlecell_utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import labkey as lk
 from lkaccess import LabKey as lka
 from skimage import transform as sktrans
+from sys import platform
+
 
 def query_data_from_labkey(cell_line_id):
 
@@ -26,26 +28,28 @@ def query_data_from_labkey(cell_line_id):
     # Query for labkey data
 
     filters = [
-            # 100X FOVs
-            lk.query.QueryFilter('Objective', '100.0', 'eq'),
-            # Production data
-            lk.query.QueryFilter('WellId/PlateId/PlateTypeId/Name', 'Production', 'contains'),
-            # Pipeline 4
-            lk.query.QueryFilter('WellId/PlateId/Workflow/Name', '4', 'contains'),
-            # Not celigo images
-            lk.query.QueryFilter('InstrumentId/Name', 'Celigo', 'neqornull'),
-            # Passed QC
-            lk.query.QueryFilter('QCStatusId/Name', 'Passed', 'eq'),
-            # Cell line (13 = Lamin)
-            lk.query.QueryFilter('SourceImageFileId/cell_line_id/Name', cell_line_id, 'contains')
+        # 100X FOVs
+        lk.query.QueryFilter('Objective', '100.0', 'eq'),
+        # Production data
+        lk.query.QueryFilter(
+            'WellId/PlateId/PlateTypeId/Name', 'Production', 'contains'),
+        # Pipeline 4
+        lk.query.QueryFilter('WellId/PlateId/Workflow/Name', '4', 'contains'),
+        # Not celigo images
+        lk.query.QueryFilter('InstrumentId/Name', 'Celigo', 'neqornull'),
+        # Passed QC
+        lk.query.QueryFilter('QCStatusId/Name', 'Passed', 'eq'),
+        # Cell line (13 = Lamin)
+        lk.query.QueryFilter(
+            'SourceImageFileId/cell_line_id/Name', cell_line_id, 'contains')
     ]
 
     query_raw = lka(host="aics").select_rows_as_list(
 
-        schema_name = "microscopy",
+        schema_name="microscopy",
         query_name="FOV",
-        filter_array = filters,
-        columns = [
+        filter_array=filters,
+        columns=[
             'FOVId',
             'PixelScaleX',
             'PixelScaleY',
@@ -57,27 +61,26 @@ def query_data_from_labkey(cell_line_id):
 
     query_seg = lka(host="aics").select_rows_as_list(               
 
-        schema_name = 'fms',
-        query_name = 'FileUI',
-        filter_array = filters + [
+        schema_name='fms',
+        query_name='FileUI',
+        filter_array=filters + [
             # Nuclei segmentation only
             lk.query.QueryFilter('ReadPath', 'nucWholeIndexImageScale', 'contains')
         ],
-        columns = [
+        columns=[
             'FOVId',
             'ReadPath',
         ]
 
     )
 
-    df_seg = pd.DataFrame(query_seg) # FOVIds are returned as list
+    df_seg = fix_filepaths(pd.DataFrame(query_seg))  # FOVIds are returned as list
     df_seg = df_seg.rename(columns={'FOVId': 'FOVIdList', 'ReadPath': 'ReadPathSeg'})
 
-    df_raw = pd.DataFrame(query_raw)
+    df_raw = fix_filepaths(pd.DataFrame(query_raw))
     df_raw = df_raw.rename(columns={'SourceImageFileId/LocalFilePath': 'ReadPathRaw'})
 
     # Merging raw and seg tables
-
     for index in df_seg.index:
         fov_id = df_seg.FOVIdList[index]
         # Some FOVs on labkey do not have segmentation available and
@@ -86,7 +89,7 @@ def query_data_from_labkey(cell_line_id):
             fov_id = fov_id[0]
         else:
             fov_id = None
-        df_seg.loc[index,'FOVId'] = fov_id
+        df_seg.loc[index, 'FOVId'] = fov_id
     df_seg = df_seg.dropna()
     df_seg = df_seg.astype({'FOVId': 'int64'})
     df_seg = df_seg.drop(columns=['FOVIdList'])
@@ -98,8 +101,45 @@ def query_data_from_labkey(cell_line_id):
 
     return df
 
-def crop_object(raw, seg, obj_label, isotropic=None):
 
+def fix_filepaths(df):
+    """
+    Checks the OS and fixes filepaths for Mac.
+    Mac users should have created a "data" directory in the repo and mounted
+    the data there using:
+        mount_smbfs //<YOUR_USERNAME>@allen/programs/allencell/data ./data/
+
+    Parameters
+    ----------
+    df: dataframe
+        dataframe containing filepaths from Labkey
+    Returns
+    -------
+    df: dataframe
+        the input dataframe with filepaths truncated for Mac users
+    """
+
+    if platform == "linux" or platform == "linux2":
+        pass
+    elif platform == "darwin":
+        # if we're in osx, we change all the read paths from
+        # /allen/programs/allencell/data/...
+        # to
+        # ./data/...
+        for column in df.columns:
+            if "Path" in column:
+                df[column] = [
+                    readpath.replace("/allen/programs/allencell/", "./")
+                    for readpath in df[column]
+                ]
+    else:
+        raise NotImplementedError(
+            "OSes other than Linux and Mac are currently not supported."
+        )
+    return df
+
+
+def crop_object(raw, seg, obj_label, isotropic=None):
 
     '''
         This function returns a cropped area around an object of interest
@@ -127,18 +167,18 @@ def crop_object(raw, seg, obj_label, isotropic=None):
     '''
 
     offset = 16
-    raw = np.pad(raw, ((0,0),(offset,offset),(offset,offset)), 'constant')
-    seg = np.pad(seg, ((0,0),(offset,offset),(offset,offset)), 'constant')
+    raw = np.pad(raw, ((0, 0), (offset, offset), (offset, offset)), 'constant')
+    seg = np.pad(seg, ((0, 0), (offset, offset), (offset, offset)), 'constant')
 
-    _, y, x = np.where(seg==obj_label)
+    _, y, x = np.where(seg == obj_label)
 
     xmin = x.min() - offset
     xmax = x.max() + offset
     ymin = y.min() - offset
     ymax = y.max() + offset
 
-    seg = seg[:,ymin:ymax,xmin:xmax]
-    raw = raw[:,ymin:ymax,xmin:xmax]
+    seg = seg[:, ymin:ymax, xmin:xmax]
+    raw = raw[:, ymin:ymax, xmin:xmax]
 
     # Resize to isotropic volume
     if isotropic is not None:
@@ -152,23 +192,23 @@ def crop_object(raw, seg, obj_label, isotropic=None):
         target_scale = 0.135
 
         output_shape = np.array([
-            sz/target_scale*dim[0],
-            sy/target_scale*dim[1],
-            sx/target_scale*dim[2]], dtype=np.int)
+            sz / target_scale * dim[0],
+            sy / target_scale * dim[1],
+            sx / target_scale * dim[2]], dtype=np.int)
 
         raw = sktrans.resize(
-            image = raw,
-            output_shape = output_shape,
-            preserve_range = True,
-            anti_aliasing = True).astype(np.uint16)
+            image=raw,
+            output_shape=output_shape,
+            preserve_range=True,
+            anti_aliasing=True).astype(np.uint16)
 
         seg = sktrans.resize(
-            image = seg,
-            output_shape = output_shape,
-            order = 0,
-            preserve_range = True,
-            anti_aliasing = False).astype(np.uint8)
+            image=seg,
+            output_shape=output_shape,
+            order=0,
+            preserve_range=True,
+            anti_aliasing=False).astype(np.uint8)
 
-    seg = (seg==obj_label).astype(np.uint8)
+    seg = (seg == obj_label).astype(np.uint8)
 
     return raw, seg
