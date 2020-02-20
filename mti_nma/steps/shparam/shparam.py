b1cee0a4c46e38761c3bcbedd6baa96b3454561f
Julie <juliec@alleninstitute.org>
2020-02-20 14:05:16 -0800

    Update to be compatible with new aicsshparam

--- mti_nma/steps/shparam/shparam.py
+++ mti_nma/steps/shparam/shparam.py
@@ -55,22 +55,29 @@ class Shparam(Step):
             seg = skio.imread(impath)
 
             # Get spherical harmonic decomposition of segmentation
-            df_coeffs, extras = aicsshparam.get_shcoeffs(
-                seg=seg,
-                params={"sigma": 1, "lmax": 8})
+            # Here is the place where I need someone taking a look at the
+            # aicsshparam package to see what is the best way to return
+            # the outputs
+            (coeffs, grid), (_, mesh_init, _, grid_init) = aicsshparam.get_shcoeffs(
+                image=seg,
+                lmax=8,
+                sigma=1)
 
-            mesh_initial = extras[1]
+            # Compute reconstruction error
+            mean_sq_error = aicsshtools.get_reconstruction_error(
+                grid_input=grid_init,
+                grid_rec=grid)
 
             # Store spherical harmonic coefficients in dataframe by cell id
-            df_coeffs = pd.DataFrame(df_coeffs, index=[CellId])
+            df_coeffs = pd.DataFrame(coeffs, index=[CellId])
             df_coeffs.index = df_coeffs.index.rename("CellId")
 
             # Mesh reconstructed with the sh coefficients
-            mesh_shparam = aicsshtools.get_reconstruction_from_dataframe(df=df_coeffs)
+            mesh_shparam = aicsshtools.get_reconstruction_from_grid(grid=grid)
 
             # Save meshes as VTK file
             aicsshtools.save_polydata(
-                mesh=mesh_initial, 
+                mesh=mesh_init, 
                 filename=str(
                     sh_data_dir / f"{CellId}.initial.vtk")
             )
@@ -91,6 +98,7 @@ class Shparam(Step):
                     "InitialMeshFilePath": sh_data_dir / f"{CellId}.initial.vtk",
                     "ShparamMeshFilePath": sh_data_dir / f"{CellId}.shparam.vtk",
                     "CoeffsFilePath": sh_data_dir / f"{CellId}.shparam.csv",
+                    "MeanSqError": mean_sq_error,
                     "CellId": CellId,
                 }, name=CellId)
             self.manifest = self.manifest.append(pdSerie)
